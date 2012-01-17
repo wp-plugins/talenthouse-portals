ceced66a0f93f390defdf5008fa10ece1d5c4614
talenthouse <talenthouse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-17 19:57:51 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/talenthouse-portals/tags/1.1@491360 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ talenthouse_portals.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Plugin Name: Talenthouse Portals
+Plugin URI: http://www.talenthouse.com/
+Description: Allows you to include the Talenthouse Portal Engagement Widget within posts and pages using a simple shortcode: [th_portal]
+Version: 1.0
+Author: Allen Oliver
+Contributor: Mike Messenger
+Author URI: http://www.talenthouse.com
+Email: wp_dev@talenthouse.com
+License: GPLv2
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+function th_portal_shortcode($args){
+	// $atts	::= key[,width]
+	// $content	::= Not applicable, no enclosing form
+	// $code	::= Not used
+	// examples:	[th_portal key="#unique_key"]
+	// 		[th_portal key="#unique_key" width=730]
+
+	$error = '';	// stores error messages, empty string when no errors occur
+	$jsfileurl = plugins_url( '/talenthouse_portals/TH.js', _FILE_ );
+
+	// Obtain the shortcode variables
+	extract( 
+		shortcode_atts( 
+			array( 
+				'key' => '',
+				'width' => ''
+			), 
+			$args
+		)
+	);
+
+	// Sanitize input:
+	// 	key is not empty
+	// 	width is 600 or 730.
+	switch ( $width ) {
+		case 'narrow':
+			$widthpx = 600;
+			break;
+		case 'wide':
+			$widthpx = 735;
+			break;
+		default:
+			$error .= "<li>width parameter not valid or missing." .
+				"<ul>accepted values for width:" . 
+				"<li>narrow</li>" .
+				"<li>wide</li></ul></li>";
+	}
+
+	if ( !isset( $key ) ) {
+		$error .= "<li>affiliate key must be provided using the KEY" . 
+			" parameter</li>";
+	} else {
+		if ( '' == trim( $key ) ) {
+			$error .= "<li>affiliate key must be provided using" .
+				" the KEY parameter</li>";
+		}
+	}
+
+	// Escape now if errors are present
+	if ( '' != trim( $error ) ) {
+		$error = '<div id="th-iframe" class="error">' .
+			'[th_portal] errors:<ul>' . $error . '</ul></div>';
+		return $error;
+	}
+
+	// No Errors so proceed
+
+	// Code to insert iframe where <div id="th-iframe"> tag resides
+	$scriptHtml = '
+	<div id="th-iframe"><script type="text/javascript">(function(){ var e = document.createElement(\'script\'); e.async = true; e.src = \''.$jsfileurl.'\'; var bd = document.getElementsByTagName(\'body\')[0]; bd.appendChild(e);}()); window.thAsyncInit = function(){ TH.iframe(\'th-iframe\', "http://www.talenthouse.com/redirect/portalci/?affiliate='.$key.'#" + document.location.href, '.$widthpx.', 600); }</script></div>';
+	
+	return $scriptHtml;
+	
+}
+
+add_shortcode('th_portal','th_portal_shortcode');
+?>
